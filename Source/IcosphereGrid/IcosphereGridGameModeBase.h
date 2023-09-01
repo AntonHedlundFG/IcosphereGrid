e3cdd2113858a0151656628a99186253bb52dc97
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2023-09-01 13:59:46 +0200

    Initial commit

---
+++ Source/IcosphereGrid/IcosphereGridGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IcosphereGridGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ICOSPHEREGRID_API AIcosphereGridGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/IcosphereGrid/Private/IcosphereGridActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IcosphereGridActor.h"
+
+// Sets default values
+AIcosphereGridActor::AIcosphereGridActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIcosphereGridActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AIcosphereGridActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
